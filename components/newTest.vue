841ae619bd57e6b03f4328913eb0155421eff51f
JuliusMG11 <33003180+JuliusMG11@users.noreply.github.com>
2021-05-09 20:01:19 +0200

    commit slider

--- components/newTest.vue
+++ components/newTest.vue
@@ -1,43 +1,88 @@
 <template>
-<div class="slider-bg">
- <div class="slides-container">
-    <div class="slides-inner">
-      <div class="slide">1</div>
-      <div class="slide">2</div>
-      <div class="slide">3</div>
-      <div class="slide">4</div>
-      <div class="slide">5</div>
-      <div class="slide">6</div>
-      <div class="slide">7</div>
-      <div class="slide">8</div>
-      <div class="slide">9</div>
-      <div class="slide">10</div>
-    </div>
-  </div>
-  
-  <div class="controls">
-    <button id="prevButton">Prev</button>
-    <button id="nextButton">Next</button>
-  </div>
+
+<div>
+      <a @click="prev">Previous</a> || <a @click="next">Next</a>
+       <transition-group name='fade' tag='div'>
+            <div
+            v-for="number in [currentNumber]"
+            :key='number'
+            >
+            <img
+            :src="currentImage"
+            v-on:mouseover="stopRotation"
+            v-on:mouseout="startRotation"
+            />
+            </div>
+        </transition-group>
 </div>
 
 </template>
 
 <script>
 import { gsap } from 'gsap'
+import TweenLite from 'gsap'
 
 export default {
-	methods: {
-		 startAnimations() { 
-		 }
-	}
+	props: [],
+	data() {
+		return {
+		
+        images: [
+          'http://julo.posunemevasvys.cz/wp-content/uploads/2021/04/1617898663.png', 
+          'https://images.pexels.com/photos/3981646/pexels-photo-3981646.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+          'http://julo.posunemevasvys.cz/wp-content/uploads/2021/04/1617898663.png',
+          'https://images.pexels.com/photos/3981646/pexels-photo-3981646.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+          'http://julo.posunemevasvys.cz/wp-content/uploads/2021/04/1617898663.png',
+
+        ],
+        currentNumber: 0,
+        timer: null
+			}
+	},
+
+	mounted() {
+    this.startRotation();
+  },
+
+  methods: {
+    startRotation() {
+      this.time = setInterval(this.next, 3000);
+    },
+
+    stopRotation() {
+      clearTimeout(this.time);
+      this.time = null;
+    },
+    next() {
+      console.log('debil');
+      this.currentNumber += 1;
+    },
+    prev() {
+      this.currentNumber -=1;
+    }
+  },
+  computed: {
+    currentImage() {
+        return this.images[Math.abs(this.currentNumber) % this.images.length];
+    }
+  }
 
 }
 </script>
 
 <style scoped>
 
-
+.fade-enter-active, .fade-leave-active {
+ transition: all 0.8s ease;
+ overflow: hidden;
+ visibility: visible;
+ opacity: 1;
+ position: absolute;
+}
+.fade-enter, .fade-leave-to {
+ opacity: 0;
+ visibility: hidden;
+}
 
 
 </style>
